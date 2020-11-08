c28099b8a594d8106c1e1957f2c785562b1c7774
melrothiago <melrothiago@gmail.com>
2020-11-08 15:33:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('sort').onclick = function(){
+    if (Math.random() >= 0.5 === true) {
+        alert("Thanos foi misericordioso.");
+      } else {
+        alert("Thanos obliterou sua existência.");
+      }
+}
